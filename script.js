e133bcdb7533d13b0943b7e9b82d8b8bcf98d3fc
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-02-03 18:00:50 -0500

    updating files

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 
-
 //lowercase letter options to use
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
 //uppercase letter options to use
-var upperCase = ['A','B','C','D','E','F','G',
-'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 //special character options to use 
-var special = ['$', '%', '&', ',', ')', '*','+', '/', '-', '.', '/', ':', ';',  '<', '=', '>', '?','@', '^', '`', '{', 
-'|', '}', '~', '!',]; 
+var special = '$%&,)*+/-./:;<=>?@^`{ |}~!'.split(''); 
 //number options to use
-var numbers = ['0','1','2','3','4','5','6','7','8','9',];
+var numbers = '0123456789'.split('');
 
-
-//1.Prompts user for data to generate password.
+//1.Prompts for user data to generate password.
 function generatePassword()  {
 //ask user for choice of password length.
 //parseInt (value, radix).
@@ -25,32 +21,29 @@ var passwordLength = parseInt
   if (isNaN(passwordLength))  {
     alert('Password must of numeric value.');
   return;
-  }
+}
 //using the pipe double sign || OR to set the number range. 
   if ((passwordLength < 8) || (passwordLength >     128)) {
     alert('Password must be between 8 - 128 characters.');
   return;
-  }
+}
   
-//using confirm to confirm characters the user wants to include the password. Example: If the user wants to include a lowercase letter, the potential Character->(lowercase) will be pushed at the end into new array->(); Using (...) spread. can use concate.
+//using confirm to confirm character types the user wants to include in the password. Using .push to push new characters into the end of a new array->(); Also using (...)spread. Can use concate.
 var useLower = confirm('Would you like to use Lowercase Characters in your password. \n Click OK!');
   if (useLower) {
       potentialChars.push(...lowerCase);
-  }
-
+}
 var useUpper = confirm('Would you like to use Uppercase Characters in your password. \n Click OK!');
   if (useUpper)  {
       potentialChars.push(...upperCase);
-  }
-
+}
 var useSpecial = confirm('Would you like to use Special Characters in your password? \n Click OK!');
   if  (useSpecial)  {
       potentialChars.push(...special);
-  }
-
+}
 var useNumbers = confirm('Would you like to use Numbers in your password? \n Click OK!');
   if  (useNumbers)  {potentialChars.push(...        numbers);
-  }
+}
 
 //Using the !Not operator and the AND Logical Operator. Can use if statements with === (strict equality)
   if (!useLower && !useUpper && !useSpecial  &&
@@ -59,14 +52,16 @@ var useNumbers = confirm('Would you like to use Numbers in your password? \n Cli
   return;
 }
 
+//Loop 
 for (var i = 0; i < passwordLength; i++) {
 
+//Using Math.random()static method to get random number and Math.floor method to round number down to nearest the number.  
   var randomChars = potentialChars[Math.floor(Math.random()*potentialChars.length)];
   userInput.push(randomChars);
 }
+//Using the join() method  to create and return a new string by concatenating all of the elements in an array. Elements can be specified in how they are joined together using commas, no commas, etc.
 let password = userInput.join('');
 return  password;
-
 }
 
 // Get references to the #generate element
